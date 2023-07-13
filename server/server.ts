6a83d7b5addcc3680cf2cd99199d6aeb42b0f8b4
NgQHung <hunghunghung273@gmail.com>
2023-07-13 12:21:34 +0200

    update

--- server/server.ts
+++ server/server.ts
@@ -37,6 +37,10 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers : X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+  );
+  res.header('HTTP/1.1 200 OK');
   next();
 });
 
