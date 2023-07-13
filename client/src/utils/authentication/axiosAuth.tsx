60c4206d2e2ff00675e378e027d66d471b96f2d8
NgQHung <hunghunghung273@gmail.com>
2023-07-13 13:22:48 +0200

    update

--- client/src/utils/authentication/axiosAuth.tsx
+++ client/src/utils/authentication/axiosAuth.tsx
@@ -6,7 +6,7 @@ export const uriBase = {
   // server: "https://zalando-be.herokuapp.com",
   // server: "http://ec2-16-171-38-72.eu-north-1.compute.amazonaws.com:8080",
   // server: "https://zalando-be.onrender.com",
-  server: "https://zalando-5-3hyjrqec8-ngqhung.vercel.app",
+  server: "https://zalando-5-qpucidyeb-ngqhung.vercel.app",
 };
 
 export const authAxios = axios.create({ baseURL: uriBase.server, withCredentials: true });
