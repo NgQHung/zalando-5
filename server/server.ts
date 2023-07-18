17681ad4dfde9b2a5c829789df59ff8cbb5a963d
NgQHung <hunghunghung273@gmail.com>
2023-07-18 13:41:23 +0200

    fix corse-1

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express, { NextFunction, Response } from 'express';
 import env from 'dotenv';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
@@ -7,10 +7,15 @@ import mongoose, { ConnectOptions, Path } from 'mongoose';
 import cookieParser from 'cookie-parser';
 import serveStatic from 'serve-static';
 import path from 'path';
+const cors_proxy = require('cors-anywhere');
+// const app = require('express-cors-anywhere');
+// import cors_proxy from 'cors-anywhere'
 var cors = require('cors');
 
-// PORT
+// Listen on a specific port via the PORT environment variable
 const PORT = process.env.PORT || 8080;
+
+const host = process.env.CLIENT_URI || '0.0.0.0';
 
 env.config({ path: path.resolve(__dirname, './.env') });
 
@@ -30,31 +35,20 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.options('/*', (_, res) => {
+app.options('/*', (_: any, res: Response) => {
   res.sendStatus(200);
 });
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(function (req, res, next) {
-  // res.header('Access-Control-Allow-Origin', '*');
+app.use(function (_req: any, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  // res.header('Access-Control-Allow-Credentials', 'true');
   // res.header('Access-Control-Allow-Origin', req.headers.origin);
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
   next();
 });
 
@@ -83,7 +77,19 @@ app.use(router);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log('listening on port ', PORT));
+    // cors_proxy.createServer({
+    //   originWhitelist: [], // Allow all origins
+    // });
+    app.listen(PORT, host, () => console.log('listening on port ', PORT));
+    // cors_proxy
+    //   .createServer({
+    //     originWhitelist: [], // Allow all origins
+    //     requireHeader: ['origin', 'x-requested-with'],
+    //     removeHeaders: ['cookie', 'cookie2'],
+    //   })
+    //   .listen(PORT, host, function () {
+    //     console.log('Running CORS Anywhere on ' + host + ':' + PORT);
+    //   });
   })
   .catch((error) => {
     console.log(error);
