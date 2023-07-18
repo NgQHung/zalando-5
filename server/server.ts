fc30a7330d38763fdfc786b4cdad78fc1f30f202
NgQHung <hunghunghung273@gmail.com>
2023-07-18 14:15:32 +0200

    change

--- server/server.ts
+++ server/server.ts
@@ -7,9 +7,6 @@ import mongoose, { ConnectOptions, Path } from 'mongoose';
 import cookieParser from 'cookie-parser';
 import serveStatic from 'serve-static';
 import path from 'path';
-const cors_proxy = require('cors-anywhere');
-// const app = require('express-cors-anywhere');
-// import cors_proxy from 'cors-anywhere'
 var cors = require('cors');
 
 // Listen on a specific port via the PORT environment variable
@@ -28,23 +25,30 @@ app.use('/dist', express.static(path.resolve(__dirname, '../client/dist')));
 // });
 app.use('/', express.static(path.join(__dirname, '../client/public/index.html')));
 
+const whitelist = [process.env.CLIENT_URI, process.env.CLIENT_URI_1];
 const corsOptions = {
-  origin: process.env.CLIENT_URI,
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+  credentials: true,
+  origin: (origin: any, callback: any) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
+
 app.use(cors(corsOptions));
 
-app.options('/*', (_, res: Response) => {
-  res.sendStatus(200);
-});
 app.use(cookieParser());
 app.use(helmet());
 
 app.use(function (_req, res: Response, next: NextFunction) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization'
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   // res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -77,19 +81,7 @@ app.use(router);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    // cors_proxy.createServer({
-    //   originWhitelist: [], // Allow all origins
-    // });
-    // app.listen(PORT, host, () => console.log('listening on port ', PORT));
-    cors_proxy
-      .createServer({
-        originWhitelist: [], // Allow all origins
-        requireHeader: ['origin', 'x-requested-with'],
-        removeHeaders: ['cookie', 'cookie2'],
-      })
-      .listen(PORT, host, function () {
-        console.log('Running CORS Anywhere on ' + host + ':' + PORT);
-      });
+    app.listen(PORT, host, () => console.log('listening on port ', PORT));
   })
   .catch((error) => {
     console.log(error);
