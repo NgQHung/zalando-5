7ee9ffb2f752fc3652fa89fc458f2074462ab9cf
NgQHung <hunghunghung273@gmail.com>
2023-07-22 00:27:48 +0200

    update

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -31,8 +31,8 @@ const authController = {
         });
       }
 
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
+      // const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       // create new user
       const newUser = new User({
