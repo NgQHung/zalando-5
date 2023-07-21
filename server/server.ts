d2747c730ecfd7a22e485245b14466864d4ce88a
NgQHung <hunghunghung273@gmail.com>
2023-07-22 01:05:02 +0200

    fix

--- server/server.ts
+++ server/server.ts
@@ -45,10 +45,10 @@ app.use(function (_req, res: Response, next: NextFunction) {
 });
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json({}));
 app.use(
   express.urlencoded({
     extended: true,
