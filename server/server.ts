ca24f72045224436cea6b0d5a0a897064e185feb
NgQHung <hunghunghung273@gmail.com>
2023-07-22 01:07:42 +0200

    update

--- server/server.ts
+++ server/server.ts
@@ -46,17 +46,11 @@ app.use(function (_req, res: Response, next: NextFunction) {
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // parse application/json
 app.use(bodyParser.json({}));
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
 /** Parse the body - middleware */
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   // res.setHeader('Cache-Control', 'max-age=1209600');
   // res.setHeader('Cache-Control', 'no-cache');
