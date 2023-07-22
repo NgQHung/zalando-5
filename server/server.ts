e5bcbb128dd47c94331ea7d205a54374fd5d90fe
NgQHung <hunghunghung273@gmail.com>
2023-07-22 23:26:06 +0200

    update

--- server/server.ts
+++ server/server.ts
@@ -49,8 +49,8 @@ app.use(function (_req, res: Response, next: NextFunction) {
 // parse application/json
 // app.use(bodyParser.json());
 /** Parse the body - middleware */
-app.use(express.json());
-app.use(express.text());
+// app.use(express.json());
+// app.use(express.text());
 // app.use(express.urlencoded({ extended: true }));
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   // res.setHeader('Cache-Control', 'max-age=1209600');
