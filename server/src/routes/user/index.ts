6056268b64158bd94d7bd4e97e6682111ea534b7
NgQHung <hunghunghung273@gmail.com>
2023-07-23 21:08:02 +0200

    fix

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import userController from '../../controllers/userController';
 // import userProductController from "../../controllers/";
 import middleware from '../../middlewares/requireAuth';
-
+import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 const userRouter = express.Router();
 
 // get all users
-userRouter.get('/all', userController.getAllUsers);
+userRouter.get(
+  '/all',
+  createProxyMiddleware({
+    target: 'https://zalando-5-be.vercel.app/v1/user/all',
+    changeOrigin: true,
+  }),
+  userController.getAllUsers
+);
 
 // delete a user by ID
 userRouter.delete('/:id', middleware.verifyTokenAndAdmin, userController.deleteUser);
@@ -31,7 +38,15 @@ userRouter.post('/:id/purchased-products/post', userController.postPurchasedProd
 userRouter.get('/:id/purchased-products', userController.getPurchasedProductsById);
 
 // get data of user
-userRouter.get('/:id/shopping-cart/products', userController.getProductsFromShoppingCartById);
+userRouter.get(
+  '/:id/shopping-cart/products',
+
+  createProxyMiddleware({
+    target: 'https://zalando-5-be.vercel.app/v1/user/:id/shopping-cart/products',
+    changeOrigin: true,
+  }),
+  userController.getProductsFromShoppingCartById
+);
 userRouter.get('/:id/liked/products', userController.getProductsFromLiked);
 
 export default userRouter;
