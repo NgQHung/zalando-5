a6446830e41d7cb20d8a2b8c0b2177ef0814e31d
NgQHung <hunghunghung273@gmail.com>
2023-07-23 21:56:40 +0200

    update

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,12 +2,20 @@ import express from 'express';
 import { getAllProducts, getProductsDetail } from '../controllers/productsController';
 import authRouter from './auth';
 import userRouter from './user';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 // import authRouter
 
 const router = express.Router();
 
 // products
-router.get('/products', getAllProducts);
+router.get(
+  '/products',
+  createProxyMiddleware({
+    target: 'https://zalando-5-be.vercel.app/products',
+    changeOrigin: true,
+  }),
+  getAllProducts
+);
 router.get('/product/:id', getProductsDetail);
 
 // authentication
