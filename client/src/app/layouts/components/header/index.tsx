eceece0e87c60df9b0436f62b6af932eb63ea2c2
NgQHung <hunghunghung273@gmail.com>
2024-09-05 20:56:51 +0200

    added code

--- client/src/app/layouts/components/header/index.tsx
+++ client/src/app/layouts/components/header/index.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   const [dataType, setDataType] = useState<string | null>("");
   const dropdown_shoppingCart = useAppSelector((state) => state.UISlice.dropdown_shoppingCart);
   const lengthAddedShoppingCart = addedShoppingCart.length;
-  const lengthAddedFavorite = addedFavorite?.length !== 0 ? addedFavorite.length : addedFavoriteProductsFromDB.length;
+  const lengthAddedFavorite = addedFavorite?.length !== 0 ? addedFavorite.length : addedFavoriteProductsFromDB?.length;
   const dispatch = useAppDispatch();
 
   const activeHandler = (type: string) => {
