56f171c109053d7fb70a11b797125642be253a72
NgQHung <hunghunghung273@gmail.com>
2024-09-05 21:28:52 +0200

    added code

--- server/server.ts
+++ server/server.ts
@@ -26,9 +26,10 @@ app.use(serveStatic('public/ftp', { index: ['default.html', 'default.htm'] }));
 app.use('/dist', express.static(path.resolve(__dirname, '../client/dist')));
 app.use('/', express.static(path.join(__dirname, '../client/public/index.html')));
 // https://zalando-clone-five.vercel.app
+// 'https://zalando-5.vercel.app/',
 const corsOptions = {
   credentials: true,
-  origin: ['https://zalando-5.vercel.app', 'http://localhost:3000'],
+  origin: ['https://zalando-5-be.vercel.app/', 'http://localhost:3000'],
 };
 
 app.use(cors(corsOptions));
