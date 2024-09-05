0d16cfd60d59214f8658b04c79290a9580d5114a
NgQHung <hunghunghung273@gmail.com>
2024-09-05 21:50:37 +0200

    added code

--- server/server.ts
+++ server/server.ts
@@ -29,7 +29,13 @@ app.use('/', express.static(path.join(__dirname, '../client/public/index.html'))
 // 'https://zalando-5.vercel.app/',
 const corsOptions = {
   credentials: true,
-  origin: ['https://zalando-5-be.vercel.app', 'http://localhost:3000'],
+  origin: 'https://zalando-5.vercel.app',
+  // [
+  //   'https://zalando-5.vercel.app',
+  //   'https://zalando-5-be.vercel.app',
+  //   'http://localhost:3000',
+  //   'https://zalando-clone-five.vercel.app',
+  // ],
 };
 
 app.use(cors(corsOptions));
