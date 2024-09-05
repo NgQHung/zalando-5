5452702d2c870b2214bae62cebdd1c5a7e926966
NgQHung <hunghunghung273@gmail.com>
2024-09-05 21:56:15 +0200

    added code

--- client/src/services/apiRequest.tsx
+++ client/src/services/apiRequest.tsx
@@ -164,7 +164,7 @@ export const getLikedProductById = async (dispatch: Dispatch, user: any) => {
     baseURL: uriBase.server,
     headers: {
       Authorization: `Bearer ${user?.accessToken}`,
-      origin: "https://zalando-5-be.vercel.app",
+      // origin: "https://zalando-5-be.vercel.app",
       "Access-Control-Allow-Origin": "https://zalando-5-be.vercel.app",
       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     },
