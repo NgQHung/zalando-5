0a34e01fcf701254a524e2b26a05b78e223e79f2
NgQHung <88713708+NgQHung@users.noreply.github.com>
2024-09-06 09:46:29 +0200

    Update index.ts

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { NextFunction, Response }  from 'express';
 import userController from '../../controllers/userController';
 // import userProductController from "../../controllers/";
 import middleware from '../../middlewares/requireAuth';
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 const userRouter = express.Router();
+
+userRouter.use(function (_req, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization'
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 // get all users
 userRouter.get('/all', middleware.verifyToken, userController.getAllUsers);
