5a347971763d2b4a5d4da2b99e6d6501299ca1c3
NgQHung <88713708+NgQHung@users.noreply.github.com>
2024-09-06 09:30:19 +0200

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -11,17 +11,6 @@ const router = express.Router();
 router.get('/products', getAllProducts);
 router.get('/product/:id', getProductsDetail);
 
-router.use(function (_req, res: Response, next: NextFunction) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization'
-  );
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 // authentication
 router.use('/v1/auth', authRouter);
 
