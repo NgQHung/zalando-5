ed01896e7965e8db07d8f8a4dfcacf4e4237025c
NgQHung <88713708+NgQHung@users.noreply.github.com>
2024-09-06 09:30:55 +0200

    Update index.ts

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, {Response, NextFunction} from 'express';
 import authController from '../../controllers/authControllers';
 import tokenController from '../../controllers/tokenController';
 import middleware from '../../middlewares/requireAuth';
 
 const authRouter = express.Router();
+
+
+authRouter.use(function (_req, res: Response, next: NextFunction) {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization'
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 authRouter.post('/register', authController.register);
 authRouter.post('/login', authController.login);
