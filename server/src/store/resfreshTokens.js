a20192f4fa3dfc09c48572f426e7a7a525fa476a
Hung Nguyen <hung.nguyen@mazars.cz>
2026-06-11 14:38:34 +0200

    removed js files

--- server/src/store/resfreshTokens.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GlobalArr = void 0;
-class GlobalArr {
-}
-exports.GlobalArr = GlobalArr;
-GlobalArr.refreshTokens = [];
-// export let refreshTokens: string[] = [];
