90dd78f5c2af4e4112d863172a1a73b48fa1a60d
Hung Nguyen <hung.nguyen@mazars.cz>
2026-06-11 16:28:26 +0200

    updated

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,21 +1,14 @@
-import express, { NextFunction, Response } from 'express';
+import express from 'express';
 import { getAllProducts, getProductsDetail } from '../controllers/productsController';
 import authRouter from './auth';
 import userRouter from './user';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-// import authRouter
 
 const router = express.Router();
 
-// products
 router.get('/products', getAllProducts);
 router.get('/product/:id', getProductsDetail);
 
-// authentication
 router.use('/v1/auth', authRouter);
-
-// user
-// router.use('/v1/user', userRouter);
-router.use(userRouter);
+router.use('/v1/user', userRouter);
 
 export default router;
--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -1,52 +1,27 @@
-import express, { NextFunction, Response }  from 'express';
+import express from 'express';
 import userController from '../../controllers/userController';
-// import userProductController from "../../controllers/";
 import middleware from '../../middlewares/requireAuth';
-import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
+
 const userRouter = express.Router();
-const url = '/v1/user'
-// const url = '/v1/user'
-userRouter.use(function (_req, res: Response, next: NextFunction) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  // res.header(
-  //   'Access-Control-Allow-Headers',
-  //   'Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization'
-  // );
-  res.header('Access-Control-Allow-Headers': 'Content-Type')
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
-// get all users
-userRouter.get(`${url}/all`, middleware.verifyToken, userController.getAllUsers);
+userRouter.get('/all', middleware.verifyToken, userController.getAllUsers);
 
-// delete a user by ID
-userRouter.delete(`${url}/:id`, middleware.verifyTokenAndAdmin, userController.deleteUser);
+userRouter.delete('/:id', middleware.verifyTokenAndAdmin, userController.deleteUser);
 
-// post data of user
-userRouter.post(`${url}/:id/shopping-cart`, middleware.verifyToken, userController.addProductToShoppingCart);
-userRouter.post(`${url}/:id/liked`, middleware.verifyToken, userController.addProductToLiked);
+userRouter.post('/:id/shopping-cart', middleware.verifyToken, userController.addProductToShoppingCart);
 
-// post data address delivery user
-userRouter.post(`${url}/:id/address-delivery`, middleware.verifyToken, userController.addAddressDeliveryUser);
+userRouter.post('/:id/liked', middleware.verifyToken, userController.addProductToLiked);
 
-// get data address delivery user
-userRouter.get(`${url}/:id/address-delivery/info`, middleware.verifyToken, userController.getAddressDeliveryById);
+userRouter.post('/:id/address-delivery', middleware.verifyToken, userController.addAddressDeliveryUser);
 
-// post data purchased products && method payment user
-userRouter.post(`${url}/:id/purchased-products/post`, middleware.verifyToken, userController.postPurchasedProductsById);
+userRouter.get('/:id/address-delivery/info', middleware.verifyToken, userController.getAddressDeliveryById);
 
-// post data added products user
-// userRouter.post('/:id/added-products/post',  userProductController.addedProduct);
+userRouter.post('/:id/purchased-products/post', middleware.verifyToken, userController.postPurchasedProductsById);
 
-// get data purchased products && method payment user
-userRouter.get(`${url}/:id/purchased-products`, middleware.verifyToken, userController.getPurchasedProductsById);
+userRouter.get('/:id/purchased-products', middleware.verifyToken, userController.getPurchasedProductsById);
 
-// get data of user
-userRouter.get(`${url}/:id/shopping-cart/products`, middleware.verifyToken, userController.getProductsFromShoppingCartById);
-userRouter.get(`${url}/:id/liked/products`,  
-  middleware.verifyToken, 
-  userController.getProductsFromLiked);
+userRouter.get('/:id/shopping-cart/products', middleware.verifyToken, userController.getProductsFromShoppingCartById);
+
+userRouter.get('/:id/liked/products', middleware.verifyToken, userController.getProductsFromLiked);
 
 export default userRouter;
